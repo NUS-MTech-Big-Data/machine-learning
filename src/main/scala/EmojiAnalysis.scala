f1b8c1e060cff41b8f95ef310280b1ea06b92717
HPBmla <bimlamadhavee@gmail.com>
2021-02-22 18:27:18 +0800

    Get the correct label for input emojis

--- src/main/scala/EmojiAnalysis.scala
+++ src/main/scala/EmojiAnalysis.scala
@@ -4,11 +4,50 @@ import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{StringType, StructType}
+
 object EmojiAnalysis {
 
   def emojiAnalysis(tweet : String) {
-    val existingSparkSession = SparkSession.builder().getOrCreate()
 
+    //val emojis="😃😜😍"
+    val inputEmoji = "ee ddd eee sss 😃😜😍";
+    val ArrayEmoji = extractEmojiFromSentence(inputEmoji)
+     println(ArrayEmoji.mkString(" "))
+    val emojiUnicodes = getUnicodeOfEmoji(ArrayEmoji)
+    emojiUnicodes.show(false)
+    val value = findEmotionLabelForEmoji(emojiUnicodes)
+    value.show(false)
+
+  }
+
+  /**
+   * extract emojis from extracted text and assign into an array
+   */
+   def extractEmojiFromSentence (twitterSentence : String) : Seq[String] = {
+    return raw"\p{block=Emoticons}".r.findAllIn(twitterSentence).toSeq
+   }
+
+  /**
+   * Get unicode for the extracted emoji
+   */
+  def getUnicodeOfEmoji (emojiArray : Seq[String]) : DataFrame = {
+    val existingSparkSession = SparkSession.builder().getOrCreate()
+    import existingSparkSession.implicits._
+    val df = emojiArray.toDF("emoji")
+    val result = df.selectExpr(
+      "emoji",
+      "'U+' || trim('0' , string(hex(encode(emoji, 'utf-32')))) as unicode"
+    )
+    result.show(false)
+    return result
+  }
+
+  /**
+   * Get emoji label from dictionary
+   */
+  def findEmotionLabelForEmoji (emojiDataFrame : DataFrame) : DataFrame = {
+
+    val existingSparkSession = SparkSession.builder().getOrCreate()
     val schema = new StructType()
       .add("unicode", dataType = StringType, nullable = false)
       .add("emotionCategory", dataType = StringType, nullable = false)
@@ -16,11 +55,9 @@ object EmojiAnalysis {
     val emojiDictionary = existingSparkSession.read.option("delimiter", ",")
       .schema(schema)
       .csv("./src/main/resources/emojiDictionary.csv")
-
-    //val emojis="😃😜😍"
-    emojiDictionary.show(false)
-    emojiDictionary.select("emotionCategory").filter("unicode == '😃'").show()
+   val emojiCategory = emojiDictionary.join(emojiDataFrame, "unicode")
+    (emojiCategory.select("emoji","unicode","emotionCategory")).show(false)
+    return  emojiCategory
   }
-
 
 }
