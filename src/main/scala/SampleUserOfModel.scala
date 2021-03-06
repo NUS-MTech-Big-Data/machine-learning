2b603e1c4ccd2f97a41c938aeb33f01833185c2e
DarDevDuck <daryltanzwothers@gmail.com>
2021-03-06 13:45:25 +0800

    Accuracy Evaluate Model

--- src/main/scala/SampleUserOfModel.scala
+++ src/main/scala/SampleUserOfModel.scala
@@ -1,5 +1,7 @@
 import com.johnsnowlabs.nlp.SparkNLP
 import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{StringType, StructType}
 
 object SampleUserOfModel {
@@ -18,7 +20,24 @@ object SampleUserOfModel {
     val mlModel = PipelineModel.read.load("../../project/v1_supervised")
 
     val results = mlModel.transform(testDataSet)
+      .select(col("sentence"),
+        col("emotion") as "label",
+        concat_ws("", col("class.result")) as "prediction")
 
-    results.select("sentence", "emotion", "class.result").show(10)
+    val emotionsMap: Map[String, Double] = Map("sadness" -> 0.0, "anger" -> 1.0, "love" -> 2.0, "surprise" -> 3.0,
+      "fear" -> 4.0, "joy" -> 5.0)
+    val toEmotionNumeric = udf((input: String) => emotionsMap.get(input))
+    spark.udf.register("toEmotionNumeric", toEmotionNumeric)
+
+    val predictionLabel = results
+      .withColumn("labelNumeric", toEmotionNumeric(col("label")))
+      .withColumn("predictionNumeric", toEmotionNumeric(col("prediction")))
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("labelNumeric")
+      .setPredictionCol("predictionNumeric")
+      .setMetricName("accuracy")
+
+    println(evaluator.evaluate(predictionLabel))
   }
 }
